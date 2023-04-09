ae5504e2ab955347e1349ebe3c1f134d25eb1015
crispusomollo <crispusomollo@gmail.com>
2023-04-09 05:29:28 -0700

    C static libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - check lowercase characters
+ * @c: the character to print
+ *
+ * Return: On success 1 else 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
