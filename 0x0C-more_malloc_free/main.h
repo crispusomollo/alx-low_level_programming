37966a4f0aa3b1fa8d8a7548361d396f06d53e62
crispusomollo <crispusomollo@gmail.com>
2023-04-13 07:30:42 -0700

    Write a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -15,6 +15,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int size1, unsigned int size2);
 
 #endif
