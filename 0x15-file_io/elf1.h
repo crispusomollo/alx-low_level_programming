663b78cf0bd665b8b918469f9fcbbe23fd26421a
crispusomollo <crispusomollo@gmail.com>
2023-05-08 06:15:04 -0700

    Write a program that displays the information contained in the ELF header at the start of an ELF file.

---
+++ 0x15-file_io/elf1.h
@@ -0,0 +1,102 @@
+#ifndef _ELF1_H
+#define _ELF1_H
+
+#include "main.h"
+
+/**
+ * check_elf - checks if the file is an ELF file
+ * @e_ident: pointer to an array containing the ELF magic numbers
+ *
+ * Description: if file is not an ELF file - exit code 98.
+ */
+ 
+__home void check_elf(unsigned char *e_ident)
+{
+	int ind;
+
+	for (ind = 0; ind < 4; ind++)
+	{
+		if (e_ident[ind] != 127 &&
+		    e_ident[ind] != 'E' &&
+		    e_ident[ind] != 'L' &&
+		    e_ident[ind] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+
+/**
+ * print_magic - prints magic number of an ELF header
+ * @e_ident: pointer to an array containing the ELF magic numbers
+ */
+
+__home void print_magic(unsigned char *e_ident)
+{
+	int ind;
+
+	printf("  Magic:   ");
+
+	for (ind = 0; ind < EI_NIDENT; ind++)
+	{
+		printf("%02x", e_ident[ind]);
+
+		if (ind == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+
+/**
+ * print_class - prints the class of an ELF header
+ * @e_ident: pointer to an array containing the ELF class
+ */
+
+__home void print_class(unsigned char *e_ident)
+{
+	printf("  Class:                             ");
+
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
+/**
+ * print_data - print the data of an ELF header
+ * @e_ident: pointer to an array containing the ELF class
+ */
+ 
+__home void print_data(unsigned char *e_ident)
+{
+	printf("  Data:                              ");
+
+	switch (e_ident[EI_DATA])
+	{
+	case ELFDATANONE:
+		printf("none\n");
+		break;
+	case ELFDATA2LSB:
+		printf("2's complement, little endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
+#endif
