5a75edd03041100910de67d3d0581363d1b1f26d
crispusomollo <crispusomollo@gmail.com>
2023-07-10 02:17:38 -0700

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+
+#include <elf.h>
+
+#define __home inline
+#define __silent __attribute__((unused))
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
