e22b76b1cbafb550fefc0a669b4df233f00186f4
Crispus Makokha Omollo <126664434+crispusomollo@users.noreply.github.com>
2023-09-08 18:10:45 +0300

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "data.h"
-#include "100-init.h"
+#include "info.h"
+#include "container.h"
 
 #define table_t hash_table_t
 #define node_t hash_node_t
